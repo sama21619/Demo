de2272dcc13a5941f9a551ab7fa1779a2b2e6bae
sangnguyen <misennguyen08@gmail.com>
2023-11-01 16:53:02 +0700

    set up theme for project

--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -1,6 +1,16 @@
 
 import { createTheme } from '@mui/material/styles';
 
-const theme = createTheme();
+const theme = createTheme({
+    typography: {
+      
+        fontSize: 16,
+        h1: {
+          fontSize: '2rem', 
+        },
+        h2: {
+          fontSize: '1.5rem', 
+      },}
+    });
 
 export default theme;
